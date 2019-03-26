36666d27d87ce4e30554a28e3322cc10e391aafc
AJ Ancheta and Alexander Kondra <aj@>
2019-03-26 10:37:47 -0700

    add boxes and highlight effect

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("button#mauve").click(function() {
+    $("body").removeClass();
+    $("body").addClass("mauve-background");
+  });
+
+  $("button#tan").click(function() {
+    $("body").removeClass();
+    $("body").addClass("tan-background");
+  });
+
+  $("button#lightblue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("lightblue-background");
+
+  });
+
+  $("p").click(function(){
+    $("#pp").toggleClass("highlight");
+
+  });
+});
